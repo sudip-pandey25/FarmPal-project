6b5d8fbdb1f099eaf71a972a483cf4c096fd7a33
Sudip Shakya <mainsudip25@gmail.com>
2024-08-06 00:48:36 +0545

    minor commit

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -4,12 +4,23 @@ const User = require("../model/user");
 const router = express.Router();
 const { upload } = require("../multer");
 const ErrorHandler = require("../utils/Errorhandler");
+const fs = require("fs");
 
 router.post("/create-user", upload.single("file"), async (req, res, next) => {
   const { name, email, password } = req.body;
   const userEmail = await User.findOne({ email });
 
   if (userEmail) {
+    const filename = req.file.filename;
+    const filePath = `uploads/${filename}`;
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ message: "Error Deleting File" });
+      } else {
+        res.json({ message: "File Deleted Succesfully" });
+      }
+    });
     return next(new ErrorHandler("user already exists", 400));
   }
 
